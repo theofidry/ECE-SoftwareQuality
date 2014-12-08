5df01edc36b49b21f3f883b50f110f3daff60c0c
Théo FIDRY <theo.fidry@gmail.com>
2014-12-08 07:19:26 +0100

    Travis config...

--- .utility/push-javadoc-to-gh-pages.sh
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "theofidry/Software-Quality" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "theofidry/Software-Quality" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Publishing javadoc...\n"
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ SOFTWARE QUALITY
 
 ECE Project for Software Quality class: program a simple landing gear system (cf. Case study).
 
+Javadoc available online [here](http://theofidry.github.io/Software-Quality).
+
